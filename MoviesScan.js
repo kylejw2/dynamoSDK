c0990974f8770a70a52bf4b7bfc2f54a64bf1b38
Kyle Wright <kylejamwright@gmail.com>
2020-10-05 13:45:29 -0600

    Step 4.3: Scan

---
+++ MoviesScan.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "https://dynamodb.us-east-2.amazonaws.com",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var params = {
+    TableName: "Movies",
+    ProjectionExpression: "#yr, title, info.rating",
+    FilterExpression: "#yr between :start_yr and :end_yr",
+    ExpressionAttributeNames: {
+        "#yr": "year",
+    },
+    ExpressionAttributeValues: {
+         ":start_yr": 1950,
+         ":end_yr": 1989 
+    }
+};
+
+console.log("Scanning Movies table.");
+docClient.scan(params, onScan);
+
+function onScan(err, data) {
+    if (err) {
+        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        // print all the movies
+        console.log("Scan succeeded.");
+        data.Items.forEach(function(movie) {
+           console.log(
+                movie.year + ": ",
+                movie.title, "- rating:", movie.info.rating);
+        });
+
+        // continue scanning if we have more movies, because
+        // scan can retrieve a maximum of 1MB of data
+        if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning for more...");
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            docClient.scan(params, onScan);
+        }
+    }
+}
