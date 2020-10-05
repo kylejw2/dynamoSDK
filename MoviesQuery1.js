8bec4806cd73ebd8ee38ec4b1440eccb8231ee34
Kyle Wright <kylejamwright@gmail.com>
2020-10-05 13:34:07 -0600

    Step 4.1: Query items with the same release year

---
+++ MoviesQuery1.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "https://dynamodb.us-east-2.amazonaws.com",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+console.log("Querying for movies from 1985.");
+
+var params = {
+    TableName : "Movies",
+    KeyConditionExpression: "#yr = :yyyy",
+    ExpressionAttributeNames:{
+        "#yr": "year"
+    },
+    ExpressionAttributeValues: {
+        ":yyyy": 1985
+    }
+};
+
+docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(function(item) {
+            console.log(" -", item.year + ": " + item.title);
+        });
+    }
+});
